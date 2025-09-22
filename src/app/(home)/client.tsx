4bde714ce1f6d633fcfcfa24948fa88679de6e23
Antonio <anon@localhost>
2025-09-22 17:14:29 +0200

    comment important NOTE

--- src/app/(home)/client.tsx
+++ src/app/(home)/client.tsx
@@ -3,6 +3,12 @@
 import { trpc } from "@/trpc/client";
 
 export const PageClient = () => {
+  /** 
+   * NOTE *********** *********** *********** 
+   * Si usas useSuspenseQuery en un Client Component, necesitas envolverlo con <Suspense> 
+   * (y opcionalmente un ErrorBoundary) y, si quieres que no suspenda al hidratar, hacer 
+   * prefetch en el servidor + hydrate.
+   */
     const [data] = trpc.hello.useSuspenseQuery({ text: "Antonio" });
   return <div>Page Client says {data.greeting}</div>;
 };
